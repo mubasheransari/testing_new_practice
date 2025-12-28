701868c5e48933f4516da56f11c35228fa5ac559
mubasheransari <mubashera38@gmail.com>
2025-12-28 17:17:51 +0500

    updtions

--- lib/Api_config/api_config.dart
+++
@@ -1,5 +0,0 @@
-class ApiConfig {
-  static const String base = 'http://54.162.208.215/backend';
-  static const String login = '$base/api/login';
-  static const String signup = '$base/api/signup';
-}
